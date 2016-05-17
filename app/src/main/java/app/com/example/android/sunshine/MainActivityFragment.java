89d50e121a9a976fe1f4e073aa266ea78cb6ccff
Manohar <kvmanohar@gmail.com>
2016-05-17 22:28:37 +0100

    Moved list view population with dummy data  to a routine.

--- app/src/main/java/app/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/android/sunshine/MainActivityFragment.java
@@ -14,8 +14,11 @@ import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
+ * http://api.openweathermap.org/data/2.5/find?q=London,uk&units=metric&appid=4000c8df847f8f70e1e052a2855da229
  */
 public class MainActivityFragment extends Fragment {
+
+    private View rootView;
 
     public MainActivityFragment() {
     }
@@ -23,15 +26,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        populateListView();
 
-        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        return rootView;
+    }
+
+    public void populateListView(){
+
         String[] forecastArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/40",
                 "Wed - Cloudy - 72/60",
                 "Thu - Asteroids - 72/60",
                 "Fri - Heavy Rain - 72/60",
-                "Sat - Help Prapped in Weather station - 72/60",
+                "Sat - Help Pepped in Weather station - 72/60",
                 "Sun - Sunny - 80/68"
         };
         List<String> weekForecast = new ArrayList<>(
@@ -53,6 +62,6 @@ public class MainActivityFragment extends Fragment {
         // Get a reference to the ListView and attach this adapter to the ListView
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-        return rootView;
     }
+
 }
